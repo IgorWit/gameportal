d10c9d3c4defb5fc16993bfaddf61dc99540f21b
IgorWit <vitruk.igor1@gmail.com>
2019-02-20 21:52:04 +0200

    first upload
    firstupload

---
+++ assets/js/modalcustom.js
@@ -0,0 +1,19 @@
+
+//take temp values for operating our elements
+var modal = document.getElementById('myModal6');
+var btn = document.getElementById('myBtn6'); 
+var span6 = document.getElementsByClassName("close6")[0];
+
+function callModal(){
+	var modal = document.getElementById('myModal6');
+	var btn = document.getElementById('myBtn6'); 
+	var span6 = document.getElementsByClassName("close6")[0];
+	modal.style.display = "block";
+}
+
+function closeModal(){
+	var modal = document.getElementById('myModal6');
+	var btn = document.getElementById('myBtn6'); 
+	var span6 = document.getElementsByClassName("close6")[0];
+	modal.style.display = "none";
+}
